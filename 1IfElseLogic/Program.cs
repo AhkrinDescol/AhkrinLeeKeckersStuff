5951abf4eead5642a0b88721ec1dc91acac22bed
AhkrinDescol <amorley525@hotmail.co.uk>
2020-09-05 13:56:13 +0100

    Added second mini-lesson on function returning, member variables, and TryParse.

--- 1IfElseLogic/Program.cs
+++ 1IfElseLogic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 // alt + 92 for \ 
 // alt + 124 for |
@@ -17,30 +18,39 @@ namespace HelloWorld
     class Program
     {
         static void Main()
-        { 
-            string wantsToEnterName;
-            Console.Write("Do you want to enter your name? (Y/N): ");
-            wantsToEnterName = Console.ReadLine();
+        {
+            double myOnePoundThing = 1.00;
+            double myOtherDouble = 3.21343;
+            Console.WriteLine("myDouble is currently: " + myOnePoundThing); //   Both do the same thing.
+            Console.WriteLine("myDouble is currently: {0}", myOnePoundThing); // ^^^
+            Console.WriteLine("myDouble rounded to two places is: {0:0.##}", myOnePoundThing);
 
-            Console.WriteLine(); // New line.
+            Console.WriteLine("myDouble is {0}, and myOtherDouble is {1}", myOnePoundThing, myOtherDouble); // Prints both doubles.
+            Console.WriteLine("myDouble is {0}, myDouble is {1}, and myDouble is {2}", myOnePoundThing, myOnePoundThing, myOnePoundThing); // Prints the same double three times.
 
-            if (wantsToEnterName == "Y" || wantsToEnterName == "y" || wantsToEnterName == "Yes") // Term: Condition
-            {
-                Console.Write("Please enter your name: ");
-                string myName = ""; // Term: Initialise (set a default at time of creation)
-                myName = Console.ReadLine();
-
-                Console.WriteLine("Hello, " + myName + "!"); // Term: Concatenate
-            }
-            else if (wantsToEnterName == "N")
-            {
-                Console.WriteLine("Fine then have it your way");
-                Console.WriteLine("Hello, Anon!");
-            }
-            else
-            {
-                Console.WriteLine("PEBKAC encountered; disengage safety!");
-            }
+            //string wantsToEnterName;
+            //Console.Write("Do you want to enter your name? (Y/N): ");
+            //wantsToEnterName = Console.ReadLine();
+            //
+            //Console.WriteLine(); // New line.
+            //
+            //if (wantsToEnterName == "Y" || wantsToEnterName == "y" || wantsToEnterName == "Yes") // Term: Condition
+            //{
+            //    Console.Write("Please enter your name: ");
+            //    string myName = ""; // Term: Initialise (set a default at time of creation)
+            //    myName = Console.ReadLine();
+            //
+            //    Console.WriteLine("Hello, " + myName + "!"); // Term: Concatenate
+            //}
+            //else if (wantsToEnterName == "N")
+            //{
+            //    Console.WriteLine("Fine then have it your way");
+            //    Console.WriteLine("Hello, Anon!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("PEBKAC encountered; disengage safety!");
+            //}
 
             Console.ReadLine(); // Pauses console.
         }
